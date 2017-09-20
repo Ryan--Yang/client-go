f5fe67e954aafdf1ea0e82e70d84f9bf6ba48674
yangrui <byan299@163.com>
2017-09-20 10:40:58 +0800

    add test

--- examples/in-cluster-client-configuration/main.go
+++ examples/in-cluster-client-configuration/main.go
@@ -38,27 +38,23 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod not found\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod\n")
-		}
-
-		time.Sleep(10 * time.Second)
+	dep, err := clientset.AppsV1beta1().Deployments().List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
 	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	for _, pod := range pods.Items {
+		err = podInterface.Delete(pod.Name, &kapi.DeleteOptions{})
+		fmt.Println("pod name: %s ", pod.Name)
+		if err != nil {
+			log.Printf("Error: %s", err)
+		}
+	}
+
 }
